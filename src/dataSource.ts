b845c16d4fb2f3c5609ff45f7b726b15d178b50b
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-16 18:54:13 +0200

    DataSource file setup + TypeOrm package

---
+++ src/dataSource.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource(
+    {
+        "type": "postgres",
+        "host": "localhost",
+        "port": 5432,
+        "username": "postgres",
+        "password": "1234",
+        "synchronize": true,
+        "entities": ["src/models/**/*"],
+        "database": "dv300_term1"
+    }
+)
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err: any) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
+export default AppDataSource
