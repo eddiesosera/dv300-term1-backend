a20bc58c9cb43ce6e2b85f2564bc2c7537c5d94f
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-20 01:21:26 +0200

    Delete Location

--- src/routes/location.route.ts
+++ src/routes/location.route.ts
@@ -26,6 +26,29 @@ locationRouter.get('/', async (req, res) => {
     }
 });
 
+// Get Single Location
+locationRouter.get('/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const location = await appDataSource.getRepository(Location)
+            .createQueryBuilder("location")
+            .leftJoinAndSelect('location.skateboards', 'skateboards')
+            .leftJoinAndSelect('location.users', 'users')
+            .where("location.id = :id", { id: id })
+            .getOne()
+
+        if (!location) {
+            return res.status(404).json({ error: 'Location not found' });
+        }
+
+        res.json(location);
+
+    } catch (error) {
+        console.log('Error fetching Location: ', error)
+        res.status(500).json({ error: 'Internal server error fetching Location' })
+    }
+});
+
 // Update Single
 locationRouter.get('/:id', async (req, res) => {
     try {
@@ -52,6 +75,28 @@ locationRouter.get('/:id', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' })
     }
 })
+
+// Delete Single Location
+locationRouter.delete('/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        // Delete Location
+        const locationDelete = await appDataSource.getRepository(Location)
+            .createQueryBuilder()
+            .delete()
+            .from(Location)
+            .where("id = :id", { id: id })
+            .execute()
+
+        res.json("Successfully removed Location. " + JSON.stringify(locationDelete))
+
+    } catch (error) {
+        console.log('Error deleting location: ', error)
+        res.status(500).json({ error: 'Internal server error while deleting location' })
+    }
+});
+
 
 export default locationRouter
 
